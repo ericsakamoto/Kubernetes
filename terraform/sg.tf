faf8cfbdb616906b15f1bb4830664a38af77b6b2
ericsakamoto <eric.sakamoto@gmail.com>
2025-06-22 21:57:51 +0000

    remove ingress rule

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -3,12 +3,12 @@ resource "aws_security_group" "ssh_sg" {
   description = "SKMT EC2 SG"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this
-  }
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this
+#  }
 
   egress {
     from_port   = 0
