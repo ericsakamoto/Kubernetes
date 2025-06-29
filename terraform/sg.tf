dfb5947561c89a31d38bc96f3336d98360dc82d1
ericsakamoto <eric.sakamoto@gmail.com>
2025-06-29 19:21:23 +0000

    add ingress rule

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -16,3 +16,12 @@ resource "aws_security_group_rule" "skmt_sg_rule_egress" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "skmt_sg_rule_ingress_ssh" {
+  security_group_id = aws_security_group.skmt_sg.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
