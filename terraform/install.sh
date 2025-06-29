ac782a70028fd72ea9c677cb7dac5a093d780100
ericsakamoto <eric.sakamoto@gmail.com>
2025-06-29 18:20:05 +0000

    add install.sh

---
+++ terraform/install.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+# Update system packages
+sudo yum update -y
+
+# Install yum-utils
+sudo yum install -y yum-utils
+
+# Add HashiCorp repository
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+
+# Install Terraform
+sudo yum install -y terraform
+
+# Verify installation
+terraform -v
