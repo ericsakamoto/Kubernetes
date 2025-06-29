ef20bfcc08180130b4c9931329768bc8357854e2
ericsakamoto <eric.sakamoto@gmail.com>
2025-06-29 22:55:04 +0000

    remove availability zone

--- terraform/network.tf
+++ terraform/network.tf
@@ -46,7 +46,7 @@ resource "aws_subnet" "skmt_public_subnet" {
   vpc_id                  = aws_vpc.skmt_vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "${var.region}a"
+  #availability_zone       = "${var.region}a"
   tags = {
     Name = "skmt_public_subnet"
   }
