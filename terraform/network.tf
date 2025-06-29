bc2818a0ebfdd406f024b718e4112de763b65a3a
ericsakamoto <eric.sakamoto@gmail.com>
2025-06-29 19:29:44 +0000

    add public subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -42,6 +42,34 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.skmt_route_table.id
 }
 
+resource "aws_subnet" "skmt_public_subnet" {
+  vpc_id                  = aws_vpc.skmt_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.region}a"
+  tags = {
+    Name = "skmt_public_subnet"
+  }
+}
+
+resource "aws_route_table" "skmt_public_route_table" {
+  vpc_id = aws_vpc.skmt_vpc.id
+  tags = {
+    Name = "skmt_public_route_table"
+  }
+}
+
+resource "aws_route" "skmt_public_route" {
+  route_table_id         = aws_route_table.skmt_public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.smkt_igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.skmt_public_subnet.id
+  route_table_id = aws_route_table.skmt_public_route_table.id
+}
+
 # VPC Endpoints for SSM
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id            = aws_vpc.skmt_vpc.id
