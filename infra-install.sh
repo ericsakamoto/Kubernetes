b5ac3c52ea02cefce78772bda117a946331f6d05
ericsakamoto <eric.sakamoto@gmail.com>
2025-08-12 11:51:40 +0000

    add installition script

---
+++ infra-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm version
+
+# Install Istio
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.22.0 sh -
+cd istio-1.22.0
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=minimal -y
+kubectl label namespace skmt istio-injection=enabled
