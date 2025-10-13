4420a34eed0bca841184d4c7e13c9e227d1915f5
ericsakamoto <eric.sakamoto@gmail.com>
2025-10-13 00:20:05 +0000

    adjust curl

--- app/call_app.sh
+++ app/call_app.sh
@@ -2,6 +2,8 @@
 PORT=$1
 INPUT=$2
 
+echo "Calling app on port ${PORT} with input ${INPUT}"
+
 curl -X POST http://192.168.49.2:${PORT}/send \
      -H "Content-Type: application/json" \
-     -d '{"value": ${INPUT}}'
+     -d "{\"value\": ${INPUT}}"
